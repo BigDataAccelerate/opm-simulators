16fc73c4efc493ffab50b25e2df885dae9074598
Tor Harald Sandve <tosa@norceresearch.no>
2023-11-14 14:06:23 +0100

    Fix connection and well dfactor
    The well dfactor is scaled by the well index
    
    If postive the connection dfactor is threated as a well factor
    and also scaled. If negative the connection dfactor is not scaled

--- opm/simulators/wells/MultisegmentWell_impl.hpp
+++ opm/simulators/wells/MultisegmentWell_impl.hpp
@@ -876,9 +876,10 @@ std::cout << "---out: MultisegmentWell::addWellContributions(..) (addWells = TRU
                 }
             }
             // injecting connections total volumerates at standard conditions
-            const Value factor = total_mob / volume_ratio;
             for (int componentIdx = 0; componentIdx < this->numComponents(); ++componentIdx) {
-                cq_s[componentIdx] = - Tw[componentIdx] * cmix_s[componentIdx] * factor * drawdown;
+                const Value cqt_i = - Tw[componentIdx] * (total_mob * drawdown);
+                Value cqt_is = cqt_i / volume_ratio;
+                cq_s[componentIdx] = cmix_s[componentIdx] * cqt_is;
             }
         } // end for injection perforations
 
