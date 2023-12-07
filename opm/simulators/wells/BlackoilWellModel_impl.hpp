e14a0629fbe7094a91bffdb8f545966d62b411a9
Markus Blatt <markus@dr-blatt.de>
2023-12-07 16:41:16 +0100

    Add semicolon to help with indentation.

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -1529,7 +1529,7 @@ std::cout << " when do we get here?? -> BlackoilWellModel<TypeTag>::linearizeDom
     {
         // We make sure that all processes throw in case there is an exception
         // on one of them (WetGasPvt::saturationPressure might throw if not converged)
-        OPM_BEGIN_PARALLEL_TRY_CATCH()
+        OPM_BEGIN_PARALLEL_TRY_CATCH();
 
         for (auto& well: well_container_) {
             well->assembleWellEqWithoutIteration(ebosSimulator_, dt, this->wellState(), this->groupState(),
